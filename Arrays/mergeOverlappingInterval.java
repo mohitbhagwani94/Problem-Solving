ca61d785fe19afaa0ce9464715806a14accf97e9
mohitbhagwani94 <mohitbhagwani94@gmail.com>
2017-07-18 23:39:54 +0530

    merge overlapping Interval

---
+++ Arrays/mergeOverlappingInterval.java
@@ -0,0 +1,54 @@
+public class Solution {
+    public ArrayList<Interval> merge(ArrayList<Interval> intervals) {
+        Collections.sort(intervals, new IntervalComp());
+        ArrayList<Interval> res;
+        Interval inter;
+        int n = intervals.size();
+        res = new ArrayList<>();
+        
+        for (int i = 0; i < n;) {
+            
+            inter = new Interval(intervals.get(i).start, intervals.get(i).end);
+            Interval next;
+            i++;
+            
+            while (i < n) {
+                next = intervals.get(i);
+                if (overlap(inter, next)) {
+                    inter.end = Math.max(inter.end, next.end);
+                    i++;
+                } else {
+                    break;
+                }
+            }
+            
+            res.add(inter);
+            
+        }
+        
+        return res;
+    }
+    
+    private boolean overlap(Interval int1, Interval int2) {
+        
+        if (int1.end >= int2.start && int1.start <= int2.end)
+            return true;
+            
+        return false;
+        
+    }
+    
+    private class IntervalComp implements Comparator<Interval> {
+        
+        @Override
+        public int compare(Interval interval1, Interval interval2) {
+            int cmp =  Integer.compare(interval1.start, interval2.start);
+            if (cmp != 0)
+                return cmp;
+            cmp = Integer.compare(interval1.end, interval2.end);
+            return cmp;
+        }
+        
+    }
+    
+}
